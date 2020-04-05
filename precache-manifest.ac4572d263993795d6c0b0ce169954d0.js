87a5b04cd9aceae0d528edbda5d6ca270750a832
Steven McIntosh <ste.mcintosh@gmail.com>
2020-04-05 17:59:35 +0100

    Updates

---
+++ precache-manifest.ac4572d263993795d6c0b0ce169954d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76eeeaf120807e5424fc3104b987abd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13c0e962a310abe6c1f3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13c0e962a310abe6c1f3",
+    "url": "/static/js/main.fe9d06ca.chunk.js"
+  },
+  {
+    "revision": "02491b30447b792ceca9",
+    "url": "/static/js/runtime-main.967b4b1c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
